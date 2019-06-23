873e3bbd7d4ae7395c37978e5a05e352bda2810d
web-yk5160 <rubystudy0123@gmail.com>
2019-06-23 14:48:47 +0900

    Implemented accept the answer as best answer

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
-Route::post('questions/{question}/answers', 'AnswersController@store')->name('answers.store');
+// Route::post('questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
+Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
